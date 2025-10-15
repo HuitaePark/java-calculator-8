d50af1ab0a6d7e24ab094bd70d5324a225fb048b
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-15 09:03:06 +0900

    feat(Delimiter): 커스텀 구분자가 없으면 기본 구분자로 문자열을 구분하는 기능 구현

--- src/test/java/calculator/domain/DelimiterTest.java
+++ src/test/java/calculator/domain/DelimiterTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DelimiterTest {
 
@@ -12,6 +14,16 @@ public class DelimiterTest {
     void separate_With_Custom_Delimiter() {
         String input = "//;\\n1;2;3";
 
+        Delimiter delimiter = new Delimiter(input);
+        String[] numbers = delimiter.getTokens();
+
+        assertThat(numbers).containsExactly("1", "2", "3");
+    }
+
+    @DisplayName("커스텀 구분자가 없다면 기본 문자열로 문자열을 분리한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1:2:3", "1,2,3", "1:2,3", "1,2:3"})
+    void separate_With_Basic_Delimiter(String input) {
         Delimiter delimiter = new Delimiter(input);
         String[] numbers = delimiter.getTokens();
 
